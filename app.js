02f761f9984a8a6ab2f5ad48bf3293d5e11e160e
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-01-28 17:36:35 +0000

    d

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-An image will appear here in 3 seconds:
-<script>
-  navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log('SW registered!', reg))
-    .catch(err => console.log('Boo!', err));
-
-  setTimeout(() => {
-    const img = new Image();
-    img.src = '/dog.svg';
-    document.body.appendChild(img);
-  }, 3000);
-</script>
-
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker.register('/sw.js')
+                                 .then((reg) => console.log('service works'))
+                                 .catch((err) => console.log('not reg'))
+}
