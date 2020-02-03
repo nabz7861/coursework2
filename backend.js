86caa56e680dbd389af6453da9b0650d0ba1564b
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-02-03 14:19:50 +0000

    new

--- backend.js
+++ backend.js
@@ -148,6 +148,17 @@ app.get('/review/:collectionName', (req, res,next) => {
     });
 })
 
+app.put('/review/:collectionName/:id', (req, res, next) => {
+    var newvalues = { $set: { topic: req.body.topic, location: req.body.location,
+        price: req.body.price, rating: req.body.rating} };
+    dbo.collection(req.params.collectionName).updateOne({ _id: ObjectId(req.params.id) },
+    newvalues,
+    { safe: true, multi: false }, (e, result) => {
+    if (e) return next(e)
+    res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
+    })
+})
+
 
 
 
