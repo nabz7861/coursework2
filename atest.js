bdff6ac4ffd35018623e0e0e18a4372d763e03f5
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-03-07 16:15:37 +0000

    s

--- atest.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-let app = express()
-
-app.get('/courses', (req, res) => {
-res.json({'topic': 'math', 'location': 'hendon', 'price': 100 })
-
-})
-app.listen(3000);
-        
